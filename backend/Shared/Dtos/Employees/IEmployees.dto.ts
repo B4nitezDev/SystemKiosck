9b51491a826710af8c320a3a3b0eb32f7e9cdd7d
b4nitez <brianebenitez@outlook.com>
2025-04-02 18:45:18 -0300

    feat: add Dtos using partial the interface entity domain

---
+++ backend/Shared/Dtos/Employees/IEmployees.dto.ts
@@ -0,0 +1,8 @@
+﻿import { IEmployee } from '../../../Domain/Interfaces/employee.interface';
+import { WithRequired } from "../../Utils/types/TypeHelpers";
+
+export type CreateEmployeeDto =
+  WithRequired<Omit<IEmployee, 'id'>, 'name' | 'password' | 'kioskId'>;
+
+
+export type EmployeeDto = IEmployee;
---
+++ backend/Shared/Utils/types/TypeHelpers.ts
@@ -0,0 +1,5 @@
+﻿export type WithRequired<T, K extends keyof T> =
+  Required<Pick<T, K>> & Partial<Omit<T, K>>;
+
+export type WithOptional<T, K extends keyof T> =
+  Partial<Pick<T, K>> & Required<Omit<T, K>>;
