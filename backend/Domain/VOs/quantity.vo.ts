52339520c1ec4f2d9bcdfa018582e45bb8d88a02
b4nitez <brianebenitez@outlook.com>
2025-04-02 18:46:50 -0300

    refact: changes name ok folder

---
+++ backend/Domain/VOs/quantity.vo.ts
@@ -0,0 +1,15 @@
+﻿export class QuantityVo {
+  private constructor(private readonly value: number) {}
+
+  public static Create(value: number): QuantityVo {
+    if(value < 0) {
+      throw new Error('QuantityVo must be greater than 0');
+    }
+
+    return new QuantityVo(value);
+  }
+
+  public GetValue(): number {
+    return this.value;
+  }
+}
